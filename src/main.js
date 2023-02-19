bb95b18ed86ca9866ffcf028f06c05b931a7290d
Ben Bushell <ben.bushell87@icloud.com>
2023-02-19 19:28:19 +0700

    add font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-createApp(App).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faGithubSquare,
+  faLinkedin,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(faGithubSquare, faLinkedin, faTwitterSquare, faEnvelope);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
