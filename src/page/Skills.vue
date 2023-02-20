0f7f1e0731ffbfd2eee182260ba1003ffb854935
Ben Bushell <ben.bushell87@icloud.com>
2023-02-20 14:42:32 +0700

    add pages

---
+++ src/page/Skills.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center shadow-2xl rounded border-solid p-20">
+    <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+      Tech
+    </h1>
+    <TechNav />
+    <div class="flex gap-3 justify-center mt-6"></div>
+  </div>
+</template>
+
+<script setup>
+import TechNav from "../components/TechNav.vue";
+</script>
