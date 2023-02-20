76b54dc96f0a3eebd4c6a8075c06974b3c1aecba
Ben Bushell <ben.bushell87@icloud.com>
2023-02-20 14:42:51 +0700

    add new component

---
+++ src/components/BackEnd.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center shadow-2xl rounded border-solid p-20">
+    <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+      Back-End
+    </h1>
+    <div>
+      <ul class="flex gap-8 justify-center mt-10">
+        <li>
+          <font-awesome-icon icon="fa-brands fa-python" class="text-6xl" />
+          <p>Python</p>
+        </li>
+        <li>
+          <font-awesome-icon icon="fa-brands fa-node-js" class="text-6xl" />
+          <p>Node JS</p>
+        </li>
+        <li>
+          <img src="../assets/psql_icon.png" alt="psql icon" class="w-16" />
+          <p>PSQL</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup></script>
---
+++ src/components/FrontEnd.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-center shadow-2xl rounded border-solid p-20">
+    <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+      Front-End
+    </h1>
+    <div>
+      <ul class="flex gap-10 justify-center mt-10 place-items-baseline">
+        <li>
+          <font-awesome-icon icon="fa-brands fa-react" class="text-6xl" />
+          <p>React</p>
+        </li>
+        <li>
+          <font-awesome-icon icon="fa-brands fa-vuejs" class="text-6xl" />
+          <p>Vue</p>
+        </li>
+        <li class="place-self-end">
+          <img src="../assets/svelte_icon.png" alt="svelte icon" class="h-14" />
+          <p>Svelte</p>
+        </li>
+        <li>
+          <font-awesome-icon icon="fa-brands fa-html5" class="text-6xl" />
+          <p>HTML5</p>
+        </li>
+        <li>
+          <font-awesome-icon icon="fa-brands fa-square-js" class="text-6xl" />
+          <p>JavaScript</p>
+        </li>
+        <li>
+          <font-awesome-icon icon="fa-brands fa-css3-alt" class="text-6xl" />
+          <p>CSS3</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup></script>
