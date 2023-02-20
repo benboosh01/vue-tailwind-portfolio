ea3b3dd8e0f79d2505563f51ac743c49ac1a35bf
Ben Bushell <ben.bushell87@icloud.com>
2023-02-20 14:40:00 +0700

    add nav buttons

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,2 +1,26 @@
-<template></template>
+<template>
+  <div class="mt-10 flex items-center justify-center gap-x-4">
+    <button
+      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-32"
+    >
+      Tech
+    </button>
+    <button
+      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-32"
+    >
+      Portfolio
+    </button>
+    <button
+      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-32"
+    >
+      Experience
+    </button>
+    <button
+      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-32"
+    >
+      Contact
+    </button>
+  </div>
+</template>
+
 <script setup></script>
