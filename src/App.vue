2c834407454a6b374dcff28fa87151c3adff1761
Ben Bushell <ben.bushell87@icloud.com>
2023-02-20 14:39:06 +0700

    update imports

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <main class="grid h-screen place-items-center">
-    <Home />
+    <FrontEnd />
   </main>
 </template>
 
 <script setup>
-import Home from "./components/Home.vue";
+import Home from "./page/Home.vue";
+import Skills from "./page/Skills.vue";
+import BackEnd from "./components/BackEnd.vue";
+import FrontEnd from "./components/FrontEnd.vue";
 </script>
 
 <style scoped></style>
