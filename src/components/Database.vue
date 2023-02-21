ed4e00069918bca89a1964e4e695aaa983b89398
Ben Bushell <ben.bushell87@icloud.com>
2023-02-21 18:24:07 +0700

    add datbase skills component

---
+++ src/components/Database.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <ul class="flex gap-8 justify-center mt-10">
+        <li class="place-self-end">
+          <img src="../assets/sql_icon.png" alt="svelte icon" class="h-14" />
+          <p>SQL</p>
+        </li>
+        <li class="place-self-end">
+          <img
+            src="../assets/supabase_icon.svg"
+            alt="svelte icon"
+            class="h-14"
+          />
+          <p>Supabase</p>
+        </li>
+        <li class="place-self-end">
+          <img
+            src="../assets/firebase_icon.png"
+            alt="svelte icon"
+            class="h-14"
+          />
+          <p>Firebase</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup></script>
