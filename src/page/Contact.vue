bdb457b54b855964afd80d8548d5314542e54184
Ben Bushell <ben.bushell87@icloud.com>
2023-02-21 18:24:49 +0700

    add new pages

---
+++ src/page/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+      Contact
+    </h1>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+      <a
+        href="mailto:ben.bushell87@icloud.com"
+        class="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 flex gap-3 place-items-center"
+        >Email
+        <font-awesome-icon class="text-2xl" icon="fa-regular fa-envelope"
+      /></a>
+    </div>
+    <div class="flex gap-3 justify-center mt-6">
+      <a
+        href="https://www.linkedin.com/in/ben-bushell-108901a2/"
+        target="_blank"
+      >
+        <font-awesome-icon
+          icon="fa-brands fa-linkedin"
+          class="text-4xl sm:text-5xl"
+      /></a>
+      <a href="https://github.com/benboosh01" target="_blank">
+        <font-awesome-icon
+          icon="fa-brands fa-square-github"
+          class="text-4xl sm:text-5xl"
+      /></a>
+      <a href="https://twitter.com/ben_boosh01" target="_blank">
+        <font-awesome-icon
+          icon="fa-brands fa-square-twitter"
+          class="text-4xl sm:text-5xl"
+      /></a>
+    </div>
+  </div>
+</template>
+<script setup></script>
