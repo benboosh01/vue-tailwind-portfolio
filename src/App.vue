372796fdc1a92d6dbc442678ba284c4346ac8381
benboosh01 <ben.bushell87@icloud.com>
2023-02-23 19:03:08 +0700

    add navigation toggle

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <Nav />
-  <main class="grid justify-items-center">
-    <div
-      class="text-center shadow-2xl rounded border-solid p-20 mt-36 grid place-items-center md:w-[768px] md:h-[468px] bg-white"
-    >
+  <button
+    to="/nav"
+    @click="toggleNav"
+    class="absolute top-5 right-12 text-4xl text-white"
+  >
+    <font-awesome-icon icon="fa-solid fa-bars" />
+  </button>
+  <div class="h-20 bg-slate-600"></div>
+  <Nav v-if="showNav" @toggleNav="toggleNav" />
+  <main class="grid justify-items-center bg-slate-100 h-full">
+    <div class="text-center p-20 mt-15 grid container">
       <router-view></router-view>
     </div>
   </main>
@@ -11,6 +17,15 @@
 
 <script setup>
 import Nav from "./components/Nav.vue";
+import { ref } from "vue";
+
+let showNav = ref(false);
+
+const toggleNav = () => {
+  showNav.value = !showNav.value;
+};
 </script>
 
-<style scoped></style>
+<style>
+@import "./style.css";
+</style>
