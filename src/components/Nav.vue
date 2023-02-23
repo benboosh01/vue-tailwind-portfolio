352f2354d08f1242d0103b2c43a5d44eadda6cb5
benboosh01 <ben.bushell87@icloud.com>
2023-02-23 19:04:13 +0700

    add styling

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="flex justify-center mt-10">
-    <nav class="flex gap-10">
-      <h1>Ben Bushell</h1>
-      <router-link to="/">Home</router-link>
-      <router-link to="/skills">Technical Skills</router-link>
-      <router-link to="/portfolio">Portfolio</router-link>
-      <router-link to="/contact">Contact</router-link>
-    </nav>
-  </div>
+  <nav
+    class="z-10 flex justify-center py-8 absolute top-20 left-0 right-0 bottom-0 bg-slate-100"
+  >
+    <div class="flex gap-8 flex-col text-3xl text-center w-11/12">
+      <router-link
+        class="bg-slate-300 py-4 rounded"
+        to="/"
+        @click="$emit('toggleNav')"
+        >Home</router-link
+      >
+      <router-link
+        class="bg-slate-300 py-4 rounded"
+        to="/skills"
+        @click="$emit('toggleNav')"
+        >Technical Skills</router-link
+      >
+      <router-link
+        class="bg-slate-300 py-4 rounded"
+        to="/portfolio"
+        @click="$emit('toggleNav')"
+        >Portfolio</router-link
+      >
+      <router-link
+        class="bg-slate-300 py-4 rounded"
+        to="/contact"
+        @click="$emit('toggleNav')"
+        >Contact</router-link
+      >
+    </div>
+  </nav>
 </template>
 
 <script setup></script>
