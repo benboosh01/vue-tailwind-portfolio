29a17f873bb51a13db4f56ace5fbacc217277f98
benboosh01 <ben.bushell87@icloud.com>
2023-02-24 14:37:28 +0700

    update styles

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,25 +4,25 @@
   >
     <div class="flex gap-8 flex-col text-3xl text-center w-11/12">
       <router-link
-        class="bg-slate-300 py-4 rounded"
+        class="bg-slate-300 py-4 rounded text-4xl font-bold"
         to="/"
         @click="$emit('toggleNav')"
         >Home</router-link
       >
       <router-link
-        class="bg-slate-300 py-4 rounded"
+        class="bg-slate-300 py-4 rounded text-4xl font-bold"
         to="/skills"
         @click="$emit('toggleNav')"
         >Technical Skills</router-link
       >
       <router-link
-        class="bg-slate-300 py-4 rounded"
+        class="bg-slate-300 py-4 rounded text-4xl font-bold"
         to="/portfolio"
         @click="$emit('toggleNav')"
         >Portfolio</router-link
       >
       <router-link
-        class="bg-slate-300 py-4 rounded"
+        class="bg-slate-300 py-4 rounded text-4xl font-bold"
         to="/contact"
         @click="$emit('toggleNav')"
         >Contact</router-link
